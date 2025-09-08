d3d8925298756c4e992192a08bced5572dc592e2
Petro <petrotymchuck@gmail.com>
2025-09-08 07:39:21 +0300

    fix cros error

--- AtbWebApi/Program.cs
+++ AtbWebApi/Program.cs
@@ -17,7 +17,7 @@ builder.Services.AddCors(options =>
     options.AddPolicy("AllowAngularApp",
         builder =>
         {
-            builder.WithOrigins("http://localhost:4200")
+            builder.WithOrigins("http://localhost:4200", "http://172.18.160.122:4200")
                    .AllowAnyHeader()
                    .AllowAnyMethod();
         });
